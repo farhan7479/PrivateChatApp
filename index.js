1026de6436434acb10f31c0b4616522dbd8511b7
farhan7479 <farhan078609@gmail.com>
2023-07-26 14:35:09 +0530

    initial-commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const http=require("http");
 const express =require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app=express();
-const port= process.env.PORT ;
+const port= process.env.PORT ||8000 ;
 
 
 const users=[{}];
@@ -32,7 +34,7 @@ io.on("connection",(socket)=>{
         io.emit('sendMessage',{user:users[id],message,id});
     })
 
-    socket.on('disconnect',()=>{
+    socket.on('disconnected',()=>{
           socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
         console.log(`user left`);
     })
